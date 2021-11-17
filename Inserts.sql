9106f9e2a2b03cbeb9a8c14bc62cfb23b8e32727
O.Nsibu <oseedoublez@gmail.comgit config --global user.email oseedoublez@gmail.comgit config --global user.email oseedoublez@gmail.com>
2021-11-17 09:12:54 +0100

    Taschelampe ergänzt

--- Inserts.sql
+++ Inserts.sql
@@ -10,7 +10,9 @@ INSERT INTO Artikel (ArtikelNr,Bezeichnung,Preis) VALUES
 (99222,'Weihnachtskugel',5.00),
 (99700,'Krawatte',30),
 (99123,'Wecker',14.49),
-(99124,'Taschenkalender',5.99);
+(99124,'Taschenkalender',5.99),
+(16206,'Taschenlampe',2.99);
+
 
 
 INSERT INTO Bestellung (KundenNr,Datum) VALUES
